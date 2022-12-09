2f6b96503ead64e0b0fbacb40e831946e7c34a1e
TuaBangka12 <raynaldimangiri12@gmail.com>
2022-12-09 20:58:22 +0700

    index "sementara"

--- back-end/app/Http/Controllers/Api/DropPointController.php
+++ back-end/app/Http/Controllers/Api/DropPointController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\DropPoint;
 
 class DropPointController extends Controller
 {
-    //
+    public function index() // Method read atau menampilkan semua data customer
+   {
+       $kota = DropPoint::all(); //  Mengambil semua data kota
+
+       if(count($paket) > 0){
+           return response([
+               'message' => 'Retrieve All Success',
+               'data' => $kota
+           ], 200);
+       }// return data semua kota dalam bentuk json
+
+       return response([
+           'message' => 'Empty',
+           'data' => null
+       ], 400);    // return message data course kosong
+   }
 }
--- back-end/app/Http/Controllers/Api/KurirController.php
+++ back-end/app/Http/Controllers/Api/KurirController.php
@@ -4,8 +4,24 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Kurir;
 
 class KurirController extends Controller
 {
-    //
+    public function index() // Method read atau menampilkan semua data customer
+    {
+        $pengantaran = Kurir::all(); //  Mengambil semua data pengantaran
+
+        if(count($pengantaran) > 0){
+            return response([
+                'message' => 'Retrieve All Success',
+                'data' => $pengantaran
+            ], 200);
+        }// return data semua pengantaran dalam bentuk json
+
+        return response([
+            'message' => 'Empty',
+            'data' => null
+        ], 400);    // return message data course kosong
+    }
 }
